0c9b493a16e61e83cf4dd55b78816fc44ee75898
yuji-hirai <holdout0521@yahoo.co.jp>
2022-04-03 09:43:23 +0900

    コンテナの内容を修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -12,7 +12,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/healthcheck', function()
+{
+    return 'health ok';
+});
 Route::get('/', function () {
     return view('welcome');
 });
