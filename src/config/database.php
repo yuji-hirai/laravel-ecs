4ea7ec544dea677770eddb7422a3dee766be8ff7
yuji-hirai <holdout0521@yahoo.co.jp>
2022-10-21 22:24:20 +0900

    redis導入

--- src/config/database.php
+++ src/config/database.php
@@ -118,30 +118,31 @@ return [
     */
 
     'redis' => [
-
         'client' => env('REDIS_CLIENT', 'phpredis'),
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
 
-        'default' => [
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_DB', '0'),
-        ],
+        'clusters' => [
 
-        'cache' => [
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1'),
+            'default' => [
+                [
+                  'host' => env('REDIS_HOST', 'localhost'),
+                  'password' => env('REDIS_PASSWORD', null),
+                  'port' => env('REDIS_PORT', 6379),
+                  'database' => 0,
+                ],
+            ],
+            
+            'cache' => [
+                [
+                  'host' => env('REDIS_HOST', '127.0.0.1'),
+                  'password' => env('REDIS_PASSWORD', null),
+                  'port' => env('REDIS_PORT', '6379'),
+                  'database' => env('REDIS_CACHE_DB', '1'),
+                ]
+            ],
         ],
-
     ],
-
 ];
